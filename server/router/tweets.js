c181d30b5140912c6a0095bc9604bb109722bc1c
Minjae Chang <minjaejchang@gmail.com>
2022-08-07 15:20:57 +1200

    Add validation for POST, PUT routes

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,7 +1,20 @@
 import express from 'express';
 import 'express-async-errors';
+import { body } from 'express-validator';
 import * as tweetController from '../controller/tweets.js';
+import { validate } from '../middleware/validator.js';
+
 const router = express.Router();
+
+// validation
+// sanitization, normalization
+const validateTweet = [
+  body('text')
+    .trim()
+    .isLength({ min: 2 })
+    .withMessage('Text should be at least 2 characters long'),
+  validate,
+];
 
 // GET /tweets
 // GET /tweets?username=:username
@@ -11,10 +24,10 @@ router.get('/', tweetController.getTweets);
 router.get('/:id', tweetController.getTweetById);
 
 // POST /tweets
-router.post('/', tweetController.createTweet);
+router.post('/', validateTweet, tweetController.createTweet);
 
 // PUT /tweets/:id
-router.put('/:id', tweetController.updateTweet);
+router.put('/:id', validateTweet, tweetController.updateTweet);
 
 // DELETE /tweets/:id
 router.delete('/:id', tweetController.deleteTweet);
